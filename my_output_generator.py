ca6c3e1f51d68aac1e7ec5bd663b403d60bac4a6
namika2000 <114719204+namika2000@users.noreply.github.com>
2023-06-14 22:54:19 +0900

    revise my_output_generator.py

--- my_output_generator.py
+++ my_output_generator.py
@@ -13,7 +13,7 @@ def generate_my_output():
         # cities = read_input("input_0.csv")
         # cities = read_input("input_" + str(i) + ".csv")
         tour = solver_greedy_with_2opt.solve(cities)
-        with open(f"sample/output_{i}.csv", "w") as f:
+        with open(f"output_{i}.csv", "w") as f:
             f.write(format_tour(tour) + "\n")
 
 
